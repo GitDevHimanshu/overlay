e58e0f41921d93cb96bc5a7b28efa5f57318f4eb
himanshu <himanshu11199@gmail.com>
2024-06-15 19:43:59 +0530

    triend to implement soket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,46 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [socket, setSocket] = useState(null)
+  const [count, setCount] = useState(0);
+  const [socket, setSocket] = useState(null);
 
-  useEffect(()=>{
-    const soket = io()
-  })
+  useEffect(() => {
+    const url = "http://localhost:3000"; 
+    const url2 = "https://overlay-be.onrender.com"
+    const socketInstance = io(url2);
+    setSocket(socketInstance);
 
-  const change = (e)=>{
-    setCount(e.target.value)
-  }
+    socket.on('send_changes',(data)=>{
+      setCount(data);
+    })
+
+    return () => {
+      if (socketInstance) socketInstance.disconnect();
+    };
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const message = "test message";
+
+    if (socket && message) {
+      socket.emit('new-message', message);
+    }
+  };
 
   return (
     <>
-     <div className='text-center bg-info'>overlay
-      <p>{count}</p>
-     </div>
-     <div className='bg-danger'>
-      <input type='text' value={count} onChange={(e)=>setCount(e.target.value)} />
-     </div>
+      <div className='text-center bg-info'>overlay
+        <p>{count}</p>
+      </div>
+      <div className='bg-danger'>
+        <input type='text' value={count} onChange={(e) => setCount(e.target.value)} />
+        <button onClick={handleSubmit}>Send</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server);
+const express = require('express')
+const cors = require('cors')
+const http = require("http")
+const app = express();
+const socketIo = require('socket.io');
+const server = http.createServer(app)
+app.use(cors({
+    origin: '*',
+}))
+
+const io = socketIo(server,{
+    cors: {
+        origin: '*',
+    }
+});
 
 io.on('connection', client => {
-  console.log("client connected")
+  console.log("client connected");
+  client.on("update_change", (data)=>{
+    io.emit('send_changes', data);
+  })
+  client.on('disconnect', () => {
+    console.log('client disconnected');
+  });
 });
 
 server.listen(process.env.PORT || 3000, ()=>{
-    console.log("server started...")
-});
+    console.log("hello this is server running...")
+})
